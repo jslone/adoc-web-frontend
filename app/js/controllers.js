cfdbf7728fdf8fbc179c38e35eccb29f0ce5fb90
Jacob M. Slone <jslone1993@gmail.com>
2014-03-12 15:37:47 -0400

    adding controller support for pretty urls for docs

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,28 @@
 /* Controllers */
 
 angular.module('adocApp.controllers', [])
+  .controller('DocCtrl', ['$scope','$location','$route','$routeParams','Doc',
+    function($scope,$location,$route,$routeParams,Doc) {
+      if($routeParams.id) {
+        $scope.doc = Doc.get({id: $routeParams.id},function(doc) {
+          //change to pretty url without reloading page
+          var lastRoute = $route.current;
+          $scope.$on('$locationChangeSuccess', function() {
+            console.log($location.path());
+            if($location.path() == '/doc/v/' + doc.fullName) {
+              $route.current = lastRoute;
+            }
+          });
+          $location.path('/doc/v/' + doc.fullName);
+        });
+      }
+      else if($routeParams.fullName) {
+        $scope.doc = {name: 'testFullName', path: 'test', fullName: 'test/testFullName',children: []};
+      }
+      else {
+        $scope.doc = {name: 'root', path: '', fullName: '', children: []};
+      }
+  }])
   .controller('DocTreeRootCtrl', ['$scope','Doc',function($scope,Doc) {
   	$scope.docs = Doc.root();
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -9,7 +9,7 @@ angular.module('adocApp.services', ['ngResource'])
   .value('version', '0.1')
   .factory('Doc', ['$resource',
   	function($resource) {
-  		return $resource('/doc/:docId', {userId: '@id'}, {
+  		return $resource('/doc/:docId', {docId: '@id'}, {
   			lookup: {method: 'GET', url: '/doc/v/:fullName'},
   			root: {method: 'GET', url: '/doc', isArray: true},
   			children: {method: 'GET', url:'/doc/children/:docId', isArray: true}
